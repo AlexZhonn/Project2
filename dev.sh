3c8eee83d2fe59d4ae3ed1917681c647e77e4329
AlexZhonn <zhuen.zhong@outlook.com>
2025-11-03 00:16:58 -0500

    switch to dfs and bfs, modify dijkstra and keep it, complete the animation

---
+++ dev.sh
@@ -0,0 +1,10 @@
+cd backend
+
+python3 -m venv venv #install venv
+
+pip install -r requirements.txt
+
+uvicorn main:app --reload # play backend
+
+# for frontend, please install live sever plugin on Visual stuidio code
+
